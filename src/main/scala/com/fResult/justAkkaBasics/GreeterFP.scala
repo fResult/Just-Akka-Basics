a45ea0e5b5c9a2ec752b18710870e3ce45feae85
fResult <styxmaz@gmail.com>
2025-03-18 01:00:05 +0700

    Create `GreeterFP` Actor as Functional-Oriented Style.

--- src/main/scala/com/fResult/justAkkaBasics/GreeterFP.scala
+++ src/main/scala/com/fResult/justAkkaBasics/GreeterFP.scala
@@ -1,8 +1,24 @@
 package com.fResult.justAkkaBasics
 
-object GreeterFP {
-  // Protocol definition doesn't change compared to Object-oriented
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
 
-  // we don't define a class extending AbstractBehavior[T],
-  // Our object will only hold behavior functions
+object GreeterFP {
+  // <- Protocol definition, the type of message(s) the actor handles
+  sealed trait GreetCommand
+  private final case class Greet(whom: String) extends GreetCommand
+  private final case class GoodBye(whom: String) extends GreetCommand
+  // Protocol definition ->
+
+  def apply(): Behavior[GreetCommand] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case Greet(whom) =>
+          context.log.info(s"Hello, $whom!")
+          Behaviors.same
+        case GoodBye(whom) =>
+          context.log.info(s"Goodbye, $whom!")
+          Behaviors.same
+      }
+    }
 }
