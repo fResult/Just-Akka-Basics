9b4b927d9ea17666e0801a032110e08576a03561
fResult <styxmaz@gmail.com>
2025-03-17 22:35:16 +0700

    Prepare `GreeterFP` object.

---
+++ src/main/scala/com/fResult/justAkkaBasics/GreeterFP.scala
@@ -0,0 +1,8 @@
+package com.fResult.justAkkaBasics
+
+object GreeterFP {
+  // Protocol definition doesn't change compared to Object-oriented
+
+  // we don't define a class extending AbstractBehavior[T],
+  // Our object will only hold behavior functions
+}
