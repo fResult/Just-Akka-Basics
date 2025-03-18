3768390b1dc14d4de62215c8362077b873eed7ed
fResult <styxmaz@gmail.com>
2025-03-18 09:05:49 +0700

    Create `Main` class.

--- src/main/scala/com/fResult/justAkkaBasics/Main.scala
+++ src/main/scala/com/fResult/justAkkaBasics/Main.scala
@@ -1,9 +1,43 @@
 package com.fResult.justAkkaBasics
 
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import com.fResult.justAkkaBasics.GreeterFP.GreetCommand
+
+import scala.io.StdIn.readLine
+import scala.util.Try
 
 object Main {
   def main(args: Array[String]): Unit = {
+    handleGreeterFp()
+    handleGreeterOop()
+  }
+
+  private def handleGreeterFp(): Unit = {
     val greeterSystem = ActorSystem(GreeterFP(), "greeter-fp")
+
+    // ! Send a message to `greeterSystem` actor
+    greeterSystem ! GreeterFP.Greet("Akka w/ FP")
+    greeterSystem ! GreeterFP.GoodBye("Akka w/ FP")
+    println(">>> Press ENTER to exit <<<")
+    Try(readLine())
+
+    // When ActorSystem.terminate is invoked, the CoordinatedShutdown process
+    // will stop actors and services in specific order.
+    greeterSystem.terminate()
+  }
+
+  private def handleGreeterOop(): Unit = {
+    val greeterSystem = ActorSystem(GreeterOOP(), "greeter-oop")
+
+    // ! Send a message to `greeterSystem` actor
+    greeterSystem ! GreeterOOP.Greet("Akka w/ OOP")
+    greeterSystem ! GreeterOOP.GoodBye("Akka w/ OOP")
+    println(">>> Press ENTER to exit <<<")
+    Try(readLine())
+
+    // When ActorSystem.terminate is invoked, the CoordinatedShutdown process
+    // will stop actors and services in specific order.
+    greeterSystem.terminate()
   }
 }
