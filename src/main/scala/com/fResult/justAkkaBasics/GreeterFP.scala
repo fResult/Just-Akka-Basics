2a351ba02f8e2a1b7e4d3047d472ced20a2272d3
fResult <styxmaz@gmail.com>
2025-03-18 08:58:00 +0700

    Refactor `GreeterFP` file.

--- src/main/scala/com/fResult/justAkkaBasics/GreeterFP.scala
+++ src/main/scala/com/fResult/justAkkaBasics/GreeterFP.scala
@@ -1,24 +1,33 @@
 package com.fResult.justAkkaBasics
 
 import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 
 object GreeterFP {
   // <- Protocol definition, the type of message(s) the actor handles
   sealed trait GreetCommand
+
   final case class Greet(whom: String) extends GreetCommand
+
   final case class GoodBye(whom: String) extends GreetCommand
   // Protocol definition ->
 
   def apply(): Behavior[GreetCommand] =
-    Behaviors.receive { (context, message) =>
+    Behaviors.receive[GreetCommand] { (context, message) =>
       message match {
-        case Greet(whom) =>
-          context.log.info(s"Hello, $whom!")
-          Behaviors.same
-        case GoodBye(whom) =>
-          context.log.info(s"Goodbye, $whom!")
-          Behaviors.same
+        case Greet(whom) => greet(whom, context)
+        case GoodBye(whom) => goodBye(whom, context)
       }
     }
+
+  private def greet(whom: String, context: ActorContext[GreetCommand]): Behavior[GreetCommand] = {
+    context.log.info(s"Hello, $whom!")
+    Behaviors.same
+  }
+
+  private def goodBye(whom: String, context: ActorContext[GreetCommand]): Behavior[GreetCommand] = {
+    context.log.info(s"Goodbye, $whom!")
+    Behaviors.same
+  }
 }
+
